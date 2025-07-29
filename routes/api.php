27a3a569d7788d8c54670fa64c8269c34662349a
Filip Ristic <fr20210298@student.fon.bg.ac.rs>
2025-07-29 22:41:40 +0200

    Kreiran routes/api.php fajl i dodate API rute za Workout, NutritionEntry i HydrationEntry kontrolere

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\WorkoutController;
+use App\Http\Controllers\API\NutritionEntryController;
+use App\Http\Controllers\API\HydrationEntryController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('workouts', WorkoutController::class);
+    Route::apiResource('nutrition-entries', NutritionEntryController::class);
+    Route::apiResource('hydration-entries', HydrationEntryController::class);
+});
