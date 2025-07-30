331a20ccb73418791b8a5f17a863bcce6de7b6ad
Filip Ristic <fr20210298@student.fon.bg.ac.rs>
2025-07-30 18:21:41 +0200

    Izmene u Kernel, config/app i API rutama

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\WorkoutController;
 use App\Http\Controllers\API\NutritionEntryController;
 use App\Http\Controllers\API\HydrationEntryController;
+use App\Http\Controllers\API\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -13,6 +17,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('workouts', WorkoutController::class);
     Route::apiResource('nutrition-entries', NutritionEntryController::class);
     Route::apiResource('hydration-entries', HydrationEntryController::class);
